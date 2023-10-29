22ee339b63889ad8ad45a3c6e2b9bcbf4185557e
José Victor <jose08.victor@gmail.com>
2023-10-29 19:09:04 -0300

    Finished project

--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -1,29 +1,32 @@
 const form = document.querySelector("#form");
 const emailInput = document.querySelector("#email");
 
-const retornButton = document.querySelector("#btn-2");
-
 const signUpForm = document.querySelector(".sign-up-form");
 const successMessage = document.querySelector(".success-message");
+
+const retornButton = document.querySelector("#btn-2");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if (emailInput.value === "" || !iseMailValid(emailInput.value)) {
-        return alert("aaaaaaaa")
-    }
-    
-    form.submit();
+    if (emailInput.value === "" || !iseMailValid(emailInput.value)) return form.classList.add("invalid")
+
+    successMessage.classList.remove("disable")
+    signUpForm.classList.add("disable")
+
+    retornButton.addEventListener("click", () => {
+        signUpForm.classList.remove("disable");
+        successMessage.classList.add("disable");
+        form.submit();
+    })
 })
 
 function iseMailValid(email) {
     const emailRegex = new RegExp(
         /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
-        //    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,14}$/
     );
 
     if (emailRegex.test(email)) return true;
-    
 
     return false;
 }
